f9d187d4edf1a985ea5c72e7716336ac37513398
Sullivanhuott <148246200+Sullivanhuott@users.noreply.github.com>
2023-12-02 15:48:57 -0500

    enemy class and image draw
    Created enemy class and two methods for movement and update. Then there is now a draw of the sprite.

---
+++ enemy.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self, position, image):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
+    def update(self):
+        self.move()
+
+    #create movement method
+    def move(self):
+        self.rect.x += 1
--- main line of code.py
+++ main line of code.py
@@ -1,28 +1,53 @@
 import pygame as pg
+from enemy import Enemy
 import constants as c
 
 # initializing
 pg.init()
 
-
 #clock creation
 clock = pg.time.Clock()
-
-
 
 # create screen dimensions and screen name
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("defence of Huottalonia")
+
+#Enemy images
+ghost_image = pg.image.load('assets/dungeon/Tiles/ghost.png').convert_alpha()
+
+#create ghost group
+enemy_group = pg.sprite.Group()
+
+
+ghost = Enemy((200,300), ghost_image)
+enemy_group.add(ghost)
+
+print(ghost)
+
 
 #title game loop
 
 #main game loop
 run = True
 while run:
+    #FPS set to 60
     clock.tick(c.FPS)
+
+    #clear screen between each loop
+    screen.fill("grey100")
+
+    #update groups
+    enemy_group.update()
+
+    #drawing part of loop
+    enemy_group.draw(screen)
+
     #event_handler
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+
+    #update display
+    pg.display.flip()
 
 pg.quit()
