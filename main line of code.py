6e32b256c02206b0400640d85d4b2a67f5f9af88
Sullivanhuott <148246200+Sullivanhuott@users.noreply.github.com>
2023-12-02 14:34:03 -0500

    Revision
    needed to redo the game, creating a rpg was going to take skills I do not have.

--- main line of code.py
+++ main line of code.py
@@ -1,44 +1,28 @@
-import sys
-import pygame
-import random
-import time
+import pygame as pg
+import constants as c
 
-pygame.init()
-
-print('\nRunning the main line of code.py.')
-print('---------------------------------')
+# initializing
+pg.init()
 
 
-#screen dimensions here
-screen_wid = 800
-screen_hgt = 600
-
-#drawing template for town
-
-screen = pygame.display.set_mode((screen_wid, screen_hgt))
-pygame.display.set_caption('Lilheims Town')
-
-def Lilheims(surface):
-    grass =pygame.image.load('../assets/town/Tiles/tile_0000.png').convert()
-
-    ''' how to make black pixels transparent, ex: grass.set_colorkey((0,0,0))'''
-    # make a flat grass back ground
-    for x in range(0, surface.get_width(), grass.get_width()):
-        for y in range(0, surface.get_height(), grass.get_height()):
-            surface.blit(grass, (x,y))
-background = screen.copy()
-Lilheims(background)
+#clock creation
+clock = pg.time.Clock()
 
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.blit(background, (0,0))
-    pygame.display.flip()
 
-print('\n--------------------')
-print('End of line.')
-pygame.quit()
-sys.exit()
+# create screen dimensions and screen name
+screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+pg.display.set_caption("defence of Huottalonia")
+
+#title game loop
+
+#main game loop
+run = True
+while run:
+    clock.tick(c.FPS)
+    #event_handler
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+pg.quit()
