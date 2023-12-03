ddfd23768f6d7f34da79f57fad631e27ec6a812a
Sullivanhuott <148246200+Sullivanhuott@users.noreply.github.com>
2023-12-03 16:37:44 -0500

    created the ally class to place them and stanardize the placement

--- main line of code.py
+++ main line of code.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from enemy import Enemy
+from allies import Ally
 from levels import Level
 import constants as c
 import math
@@ -17,19 +18,31 @@ pg.display.set_caption("Defence of Huottalonia")
 #loading in the map image I created
 Huottalonia_image = pg.image.load('assets/huottalonia_gate_map.png').convert_alpha()
 
+#Ally images
+grunt_image = pg.image.load('assets/dungeon/Tiles/grunt.png').convert_alpha()
+
 #Enemy images
 ghost_image = pg.image.load('assets/dungeon/Tiles/ghost.png').convert_alpha()
 
+def create_ally(mouse_position):
+    mouse_tile_x = mouse_position[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_position[1] // c.TILE_SIZE
+    Grunt = Ally(grunt_image, mouse_tile_x, mouse_tile_y)
+    ally_group.add(Grunt)
+
 #create level
 Huottalonia = Level(Huottalonia_image)
-#create ghost group
+#create enemy group
 enemy_group = pg.sprite.Group()
-
+#create ally group
+ally_group = pg.sprite.Group()
 waypoints = [
-    (100,100),
-    (400,200),
-    (400,100),
-    (200,300)
+    (72,496),
+    (72,328),
+    (376,328),
+    (376,169),
+    (231,169),
+    (231,0)
 ]
 
 ghost = Enemy(waypoints, ghost_image)
@@ -52,19 +65,25 @@ while run:
     #draw level
     Huottalonia.draw(screen)
 
-    #illustrate enemy path
-    pg.draw.lines(screen, "grey0", False, waypoints)
-
     #update groups
     enemy_group.update()
 
     #drawing part of loop
     enemy_group.draw(screen)
+    ally_group.draw(screen)
 
     #event_handler
     for event in pg.event.get():
+        #quit function
         if event.type == pg.QUIT:
             run = False
+        #placing ally mouse event
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_position = pg.mouse.get_pos()
+            # check if mouse is on the map by limiting the placement to below the x distance of the
+            # game width and the map height
+            if mouse_position[0] < c.SCREEN_WIDTH and mouse_position[1] < c.SCREEN_HEIGHT:
+                create_ally(mouse_position)
 
     #update display
     pg.display.flip()
